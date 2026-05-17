fd300f72c81ec0ec06d2cc9e22d4db5580109e4c
Shuvo <endshuvo@gmail.com>
2026-05-17 22:35:47 +0600

    Add files via upload

---
+++ api.ts
@@ -0,0 +1,13 @@
+import * as InitialData from '../initialData';
+export * from '../types';
+
+export const PRODUCTS = InitialData.INITIAL_PRODUCTS;
+export const ORDERS = InitialData.INITIAL_ORDERS;
+export const CATEGORIES = InitialData.INITIAL_CATEGORIES;
+export const BANNER_SLIDES = InitialData.INITIAL_BANNER_SLIDES;
+export const CONTACT_LINKS = InitialData.INITIAL_CONTACT_LINKS;
+export const CATEGORY_ADS = InitialData.INITIAL_CATEGORY_ADS;
+export const STORE_LOGO = InitialData.INITIAL_STORE_LOGO;
+export const HERO_BANNER = InitialData.INITIAL_HERO_BANNER;
+export const STATIC_BANNER = InitialData.INITIAL_STATIC_BANNER;
+export const INITIAL_LAYOUT_CONFIG = InitialData.INITIAL_LAYOUT_CONFIG;
